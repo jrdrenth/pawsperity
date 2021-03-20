b63dcc0be9d2bad6a080630dc6d55cc072cb4612
Jonathan Soukaseume <sksmejn@gmail.com>
2021-03-20 13:15:40 -0700

    testing connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      port: process.env.DB_PORT,
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(
+          process.env.DB_NAME,
+          process.env.DB_USER,
+          process.env.DB_PASSWORD,
+          {
+              host: process.env.DB_HOST,
+              dialect: "mysql",
+              port: process.env.DB_PORT,
 
-      // Prevents mysql2 from converting decimal to string to preserve precision,
-      // we will not run into precision issues so it's ok to opt out of this default behavior
-      dialectOptions: { decimalNumbers: true }
-    });
+              // Prevents mysql2 from converting decimal to string to preserve precision,
+              // we will not run into precision issues so it's ok to opt out of this default behavior
+              dialectOptions: { decimalNumbers: true },
+          }
+      );
 
 module.exports = sequelize;
+// hello
