4deff528034bb8994cdf5b838451e148b8a1222f
Jonathan Soukaseume <sksmejn@gmail.com>
2021-03-20 14:16:47 -0700

    added dotenv

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const app = express();
 const hbs = require("express-handlebars");
-const sequelize = require("./config/connection");
 
 // PORT
 const PORT = process.env.PORT || 3001;
