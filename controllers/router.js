098d8587c9fa95036b926430b7f4f24c41cde970
Jonathan Soukaseume <sksmejn@gmail.com>
2021-03-21 22:02:33 -0700

    added page headers to all pages and created variable pageHeader to dynamically change each pageheaders

--- controllers/router.js
+++ controllers/router.js
@@ -3,22 +3,34 @@ const router = express.Router();
 
 // Pets page
 router.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {
+        title: "Pets",
+        pageHeader: "Your Family List",
+    });
 });
 
 // todo page
 router.get("/todo", (req, res) => {
-    res.render("todo");
+    res.render("todo", {
+        title: "Todo",
+        pageHeader: "Todo List",
+    });
 });
 
 // visit page
 router.get("/visit", (req, res) => {
-    res.render("visit");
+    res.render("visit", {
+        title: "Visits",
+        pageHeader: "Visit List",
+    });
 });
 
 // Settings page
 router.get("/settings", (req, res) => {
-    res.render("settings");
+    res.render("settings", {
+        title: "Config",
+        pageHeader: "Settings",
+    });
 });
 
 router.get("/setting", (req, res) => {
