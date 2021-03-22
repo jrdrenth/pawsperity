2d1cbf4c3f22b8b21644b750c70bd5bf4873461e
Jonathan Soukaseume <sksmejn@gmail.com>
2021-03-21 19:29:45 -0700

    added a line

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const hbs = require("express-handlebars");
 const router = require("./controllers/router");
+const sequelize = require("./config/connection");
 
 // PORT
 const PORT = process.env.PORT || 3001;
