71abc00a169897b9998583380158b92cb3b92c18
Jonathan Soukaseume <sksmejn@gmail.com>
2021-03-24 12:38:15 -0700

    added visit page and animations and changed password fields to show dots instead of displaying characters

---
+++ public/js/visitCSS.js
@@ -0,0 +1,16 @@
+// Animations for the visits page
+
+// Today animations
+$("#visit-today").click(() => {
+    $("#visit-today .hide").slideToggle();
+});
+
+// Upcomfing animations
+$("#visit-upcoming").click(() => {
+    $("#visit-upcoming .hide").slideToggle();
+});
+
+// Past animations
+$("#visit-past").click(() => {
+    $("#visit-past .hide").slideToggle();
+});
