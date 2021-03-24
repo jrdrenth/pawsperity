c711cfaa5dc2ffd16badeb1c20df15d77ce48702
jrdrenth <john.drenth@gmail.com>
2021-03-23 23:55:31 -0700

    todoRoutes.js

--- controllers/api/todoRoutes.js
+++ controllers/api/todoRoutes.js
@@ -21,22 +21,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-// router.put('/todo/:id', withAuth, async (req, res) => {
-//   try {
-//     const [affectedRows] = await Post.update(req.body, {
-//       where: {
-//         id: req.params.id,
-//       },
-//     });
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const [affectedRows] = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
 
-//     if (affectedRows > 0) {
-//       res.status(200).end();
-//     } else {
-//       res.status(404).end();
-//     }
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
