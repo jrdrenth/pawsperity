e0cdf42f9f073fd9af2ca2c29434ef94174c4c15
jrdrenth <john.drenth@gmail.com>
2021-03-23 21:43:03 -0700

    updated api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-// const userRoutes = require('./user-routes.js');
+const userRoutes = require('./userRoutes');
 const todoRoutes = require('./todoRoutes');
 // const commentRoutes = require('./comment-routes');
 
-// router.use('/user', userRoutes);
-router.use('/todo', todoRoutes);
+router.use('/users', userRoutes);
+router.use('/todos', todoRoutes);
 // router.use('/comment', commentRoutes);
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,22 +1,37 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// router.post('/', async (req, res) => {
+//   try {
+//     const newUser = await User.create({
+//       username: req.body.username,
+//       password: req.body.password,
+//     });
+
+//     req.session.save(() => {
+//       req.session.userId = newUser.id;
+//       req.session.username = newUser.username;
+//       req.session.loggedIn = true;
+
+//       res.json(newUser);
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 router.post('/', async (req, res) => {
   try {
-    const newUser = await User.create({
-      username: req.body.username,
-      password: req.body.password,
-    });
+    const newUser = await User.create(req.body);
 
     req.session.save(() => {
-      req.session.userId = newUser.id;
-      req.session.username = newUser.username;
-      req.session.loggedIn = true;
+      req.session.user_id = newUser.id;
+      req.session.logged_in = true;
 
-      res.json(newUser);
+      res.status(200).json(newUser);
     });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
