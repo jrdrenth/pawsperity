65758ade3615f5df497f0f0012c4b0934bb4a1d2
Jonathan Soukaseume <sksmejn@gmail.com>
2021-03-27 10:00:29 -0700

    added document ready

--- public/js/petdetails.js
+++ public/js/petdetails.js
@@ -1,34 +1,36 @@
-// opens confirmation box
-$("#remove-pet").click(() => {
-    $("#delete-container").slideToggle();
-    $("#remove-pet").slideUp();
-});
-
-// Cancels confirmation box
-$("#cancel-option").click(() => {
-    $("#delete-container").slideUp();
-    $("#remove-pet").slideToggle();
-});
-
-// actual removal from database
-const remove = async (e) => {
-    e.preventDefault();
-    // grabs id of the target event
-    const id = document
-        .querySelector("#delete-container")
-        .getAttribute("data-id");
-    // runs api delete
-    const response = await fetch(`/api/pets/${id}`, {
-        method: "DELETE",
+$(document).ready(() => {
+    // opens confirmation box
+    $("#remove-pet").click(() => {
+        $("#delete-container").slideToggle();
+        $("#remove-pet").slideUp();
     });
 
-    if (response.ok) {
-        // Redirect to hom page
-        document.location.replace("/");
-    } else {
-        alert("ERR: Failed to remove pet");
-    }
-};
+    // Cancels confirmation box
+    $("#cancel-option").click(() => {
+        $("#delete-container").slideUp();
+        $("#remove-pet").slideToggle();
+    });
 
-// On click action
-$("#confirm-option").click(remove);
+    // actual removal from database
+    const remove = async (e) => {
+        e.preventDefault();
+        // grabs id of the target event
+        const id = document
+            .querySelector("#delete-container")
+            .getAttribute("data-id");
+        // runs api delete
+        const response = await fetch(`/api/pets/${id}`, {
+            method: "DELETE",
+        });
+
+        if (response.ok) {
+            // Redirect to hom page
+            document.location.replace("/");
+        } else {
+            alert("ERR: Failed to remove pet");
+        }
+    };
+
+    // On click action
+    $("#confirm-option").click(remove);
+});
